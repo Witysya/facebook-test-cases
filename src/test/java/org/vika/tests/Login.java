75792cd0271dd5573ba549fc1e02ed727b5e6432
Victoria Horditsa <v.horditsa@gmail.com>
2018-02-18 23:57:43 +0200

    Fixed formatting

--- src/test/java/org/vika/tests/Login.java
+++ src/test/java/org/vika/tests/Login.java
@@ -3,8 +3,6 @@ import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.vika.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 
@@ -28,18 +26,8 @@ public class Login {
         driver.get("https://www.facebook.com");
     }
 
-//    @After
-//    public void afterEachTest() {
-//        //driver.close();
-//    }
-
     @Test
     public void testFacebookLogin_validData() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("test-type");
-        options.addArguments("disable-popup-blocking");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("witysya@i.ua");
@@ -64,17 +52,16 @@ public class Login {
         loginPage.selectSexRadioButton();
 
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         WebElement lastNameValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
-        String expectedAlert = lastNameValidationAlert.getText();  //firstNameValidationAlert
-        Assert.assertEquals("What’s your name?", expectedAlert);
+        String expectedAlert = lastNameValidationAlert.getText();
 
+        Assert.assertEquals("What’s your name?", expectedAlert);
     }
 
     @Test
     public void testFacebookLoginPage_emptyFirstName() {
-
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
         loginPage.fillEmailfield2("nalox@ugimail.net");
@@ -85,15 +72,16 @@ public class Login {
         loginPage.selectSexRadioButton();
 
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement firstNameValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
-        String expectedAlert = firstNameValidationAlert.getText();  //firstNameValidationAlert
+        String expectedAlert = firstNameValidationAlert.getText();
+
         Assert.assertEquals("What’s your name?", expectedAlert);
     }
+
     @Test
     public void testFacebookLoginPage_invalidConfirmationEmail() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillPassword("15111995");
@@ -103,19 +91,18 @@ public class Login {
         loginPage.selectSexRadioButton();
         loginPage.fillEmailfield("nalox@ugimail.net");
         loginPage.fillEmailfield2("nalodx@ugimail.net");
-        loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         loginPage.submit();
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement confirmEmailValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = confirmEmailValidationAlert.getText();
+
         Assert.assertEquals("Your emails do not match. Please try again.", expectedAlert);
     }
+
     @Test
     public void testFacebookLoginPage_emptyEmail() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillPassword("15111995");
@@ -124,19 +111,19 @@ public class Login {
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement emptyEmailValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
-        String expectedAlert = emptyEmailValidationAlert.getText();  //firstNameValidationAlert
+        String expectedAlert = emptyEmailValidationAlert.getText();
+
         Assert.assertEquals("You'll use this when you log in and if you ever need to reset your password.", expectedAlert);
     }
+
     @Test
     public void testFacebookLoginPage_emptyPassword() {
-
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -145,19 +132,18 @@ public class Login {
         loginPage.birthMonth("Nov");
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
-        loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement passwordValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
-        String expectedAlert = passwordValidationAlert.getText();  //firstNameValidationAlert
+        String expectedAlert = passwordValidationAlert.getText();
+
         Assert.assertEquals("Enter a combination of at least six numbers, letters and punctuation marks (like ! and &).", expectedAlert);
     }
+
     @Test
     public void testFacebookLogin_invalidDayInBirthday() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -167,16 +153,18 @@ public class Login {
         loginPage.birthMonth("Nov");
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement birthDayValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = birthDayValidationAlert.getText();
+
         Assert.assertEquals("Select your birthday. You can change who can see this later.", expectedAlert);
     }
+
     @Test
     public void testFacebookLogin_invalidMonthInBirthday() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -186,16 +174,18 @@ public class Login {
         loginPage.birthMonth("Month");
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement birthMonthValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = birthMonthValidationAlert.getText();
+
         Assert.assertEquals("Select your birthday. You can change who can see this later.", expectedAlert);
     }
+
     @Test
     public void testFacebookLogin_invalidYearInBirthday() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -205,17 +195,18 @@ public class Login {
         loginPage.birthMonth("Nov");
         loginPage.birthYear("Year");
         loginPage.selectSexRadioButton();
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement birthYearValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = birthYearValidationAlert.getText();
+
         Assert.assertEquals("Select your birthday. You can change who can see this later.", expectedAlert);
     }
-    //
+
     @Test
     public void testFacebookLogin_emptyRadioButton() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -224,16 +215,18 @@ public class Login {
         loginPage.birthDay("15");
         loginPage.birthMonth("Nov");
         loginPage.birthYear("1995");
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement radioButtonValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = radioButtonValidationAlert.getText();
+
         Assert.assertEquals("Please choose a gender. You can change who can see this later.", expectedAlert);
     }
+
     @Test
     public void testFacebookLogin_invalidNames() {
-
         loginPage.fillFirstname("1234");
         loginPage.fillLastname("1234");
         loginPage.fillEmailfield("nalox@ugimail.net");
@@ -243,16 +236,18 @@ public class Login {
         loginPage.birthMonth("Nov");
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         WebElement namesValidationAlert = driver.findElement(By.id ("reg_error_inner"));
         String expectedAlert = namesValidationAlert.getText();
+
         Assert.assertTrue("You have entered an invalid email. Please check your email address and try again.", expectedAlert.contains(expectedAlert));
     }
+
     @Test
     public void testFacebookLogin_invalidEmail() {
-
         loginPage.fillFirstname("Vika");
         loginPage.fillLastname("Horditsa");
         loginPage.fillEmailfield("naloxugimail.net");
@@ -261,11 +256,14 @@ public class Login {
         loginPage.birthMonth("Nov");
         loginPage.birthYear("1995");
         loginPage.selectSexRadioButton();
+
         loginPage.submit();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         WebElement emailValidationAlert = driver.findElement(By.cssSelector("._5633._5634._53ij"));
         String expectedAlert = emailValidationAlert.getText();
+
         Assert.assertEquals("Please enter a valid mobile number or email address.", expectedAlert);
     }
+
 }
